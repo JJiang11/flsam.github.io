0725b00607d12e30b1a74e0f1e9112c3823e672b
Jay-Mithani <jayvmithani@gmail.com>
2016-11-26 14:34:35 -0500

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -13,7 +13,7 @@ function showDivs(i, n) {
   var x = document.getElementsByClassName("mySlides" + i);
   if (n > x.length) {slideIndex[i] = 1}    
   if (n < 1) {slideIndex[i] = x.length}
-  for (var j = 0; i < x.length; j++) {
+  for (var j = 0; j < x.length; j++) {
      x[j].style.display = "none";  
   }
   x[slideIndex[i]-1].style.display = "block";  
